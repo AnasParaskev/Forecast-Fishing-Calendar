a061701568eae63f5c98c54c329b31355bd1dcba
AnastasiosParaskevopoulos <aparaskevdev@gmail.com>
2023-10-10 21:06:11 +0300

    First basic setup

---
+++ src/main/java/com/AnastasiosParaskevopoulos/ForecastFishingCalendar/ForecastFishingCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.AnastasiosParaskevopoulos.ForecastFishingCalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForecastFishingCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForecastFishingCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AnastasiosParaskevopoulos/ForecastFishingCalendar/ForecastFishingCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AnastasiosParaskevopoulos.ForecastFishingCalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForecastFishingCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
